bdc926ff9e67cfee592d791810853643d3ad52b8
John McNeil <john@johnmcneil.me>
2021-06-12 16:41:50 -0500

    spacing change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <h1>John McNeil</h1>
         <p>Welcome! My Github page is devoted to my work in web development.</p>
         <p>For my full portfolio website please visit <a href="https://www.johnmcneil.me">johnmcneil.me</a></p>
+        
       </header>
       <div className="App-body">
       </div>
