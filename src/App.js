a447163a767cfc92255b245fd0e387821882d3e5
John McNeil <john@johnmcneil.me>
2021-06-13 21:27:44 -0500

    copy edit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <p>I'm a full-stack web developer.</p>
       <p>I code HTML5, CSS3, JavaScript (including React, JQuery, d3, Bootstrap, Highcharts, and other libraries), PHP, Python, R, and MySQL.</p>
       <p> I also develop and manage WordPress sites, and I like to make data visualizations for the web.</p>
-      <p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me.</a></p>
+      <p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me</a>.</p>
       <iframe title="chessgraphs.com" src="https://www.chessgraphs.com" width="100%" height="600px"></iframe>
     </div>
   );
