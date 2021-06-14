3eeefb4df3e9a829c5a2012285b5d279477eaa27
John McNeil <john@johnmcneil.me>
2021-06-13 21:02:20 -0500

    moved content from old github page to new react page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>John McNeil</h1>
-        <p>Welcome! My Github page is devoted to my work in web development.</p>
-        <p>For my full portfolio website please visit <a href="https://www.johnmcneil.me">johnmcneil.me</a></p>
-        
+        <p>Welcome to my GitHub page.</p>      
       </header>
-      <div className="App-body">
-      </div>
+      <p>I'm a full-stack web developer.</p>
+      <p>I code HTML5, CSS3, JavaScript (including React, JQuery, d3, Bootstrap, Highcharts, and other libraries), PHP, Python, R, and MySQL.</p>
+      <p> I also develop and manage WordPress sites, and I like to make data visualizations for the web.</p>
+      <p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me.</a></p>
+      <iframe src="https://www.chessgraphs.com" width="100%" height="600px"></iframe>
     </div>
-
   );
 }
 
