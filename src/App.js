ac1e34c1fc6323c8711aef66f534c8eb8401a307
John McNeil <john@johnmcneil.me>
2021-06-16 12:51:41 -0500

    added Router, pages/ and HomePage component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-// import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>John McNeil</h1>
-        <p>Welcome to my GitHub page.</p>      
-      </header>
-      <p>I'm a full-stack web developer.</p>
-      <p>I code HTML5, CSS3, JavaScript (including React, jQuery, d3, Bootstrap, and other libraries), PHP, Python, R, and MySQL.</p>
-      <p> I also develop and manage WordPress sites, and I like to make data visualizations for the web.</p>
-      <p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me</a>.</p>
-      <iframe title="chessgraphs.com" src="https://www.chessgraphs.com" width="100%" height="600px"></iframe>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+	<>
+		<h1>John McNeil</h1>
+    	<p>Welcome to my GitHub page.</p>
+    	<p>I'm a full-stack web developer.</p>
+      	<p>I code HTML5, CSS3, JavaScript (including React, jQuery, d3, Bootstrap, and other libraries), PHP, Python, R, and MySQL.</p>
+      	<p> I also develop and manage WordPress sites, and I like to make data visualizations for the web.</p>
+      	<p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me</a>.</p>
+      	<iframe title="chessgraphs.com" src="https://www.chessgraphs.com" width="100%" height="600px"></iframe>
+	</>
+);
+
+export default HomePage;
