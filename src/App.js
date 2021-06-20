d02a16fcca15ef435ff4065ba15fe1c96e3fcda3
John McNeil <john@johnmcneil.me>
2021-06-19 22:59:41 -0500

    commented out the navbar for now in App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-    <NavBar />
+  {/*    <NavBar /> */}
       <div className="App">
         <Route path="/" component={HomePage} exact />
         <Route path="/about" component={AboutPage} />
