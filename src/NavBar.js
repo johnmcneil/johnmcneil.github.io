acedd975249ffc53d697a9f89301ba9fa76be71d
John McNeil <john@johnmcneil.me>
2021-06-27 19:41:45 -0500

    added Bootstrap and used it to style the navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-	<nav>
-		<ul>
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/about">About</Link>
-			</li>
-			<li>
-				<Link to="/articles-list">Articles</Link>
-			</li>
-		</ul>
+	<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+		<div className="container-fluid">
+			<a className="navbar-brand" href="#">John McNeil</a>
+			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link className="nav-link" to="/">Home</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link" to="/about">About</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link" to="/articles-list">Portfolio</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</nav>
 );
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const HomePage = () => (
 	<>
-		<h1>John McNeil</h1>
     	<p>Welcome to my GitHub page.</p>
-    	<p>I'm a full-stack web developer.</p>
-      	<p>I code HTML5, CSS3, JavaScript (including React, jQuery, d3, Bootstrap, and other libraries), PHP, Python, R, and MySQL.</p>
-      	<p> I've also developed and managed WordPress sites, and I like to make data visualizations for the web.</p>
+    	<p>I do front-end and full-stack web development, coding HTML5, CSS3, JavaScript (including React, jQuery, d3, Bootstrap, and other libraries), PHP, Python, R, and MySQL.</p>
+      	<p> I'm also experienced in developing and managing WordPress sites, and enjoy making data visualizations for the web.</p>
       	<p>Here's a sample of my work. For my full portfolio, visit <a href="https://www.johnmcneil.me">johnmcneil.me</a>.</p>
       	<iframe title="chessgraphs.com" src="https://www.chessgraphs.com" width="100%" height="600px"></iframe>
 	</>
