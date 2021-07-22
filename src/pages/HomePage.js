49ae225e750415db650776cfe324c23e8b15b789
John McNeil <john@johnmcneil.me>
2021-07-22 15:36:36 -0500

    updated portfolio to portfolioScroll in app.css and removed comment block for old portfolio in Homepage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,33 +16,7 @@ const HomePage = () => (
 			<h2>{title}</h2>
 			<h3>{subtitle}</h3>
 		</div>
-		<PortfolioScroll />
-{/*	
-		<div id="portfolio" className="row">
-			<div className="img-container col-md-6 offset-md-3">
-				<div><a href="https://www.chessgraphs.com">
-					<img src={chessgraphs} />
-				</a></div>
-				<div><a href="https://www.johnmcneil.me/ajax-news/">
-					<img src={johnmcneilme} />
-				</a></div>
-				<div><a href="https://github.com/johnmcneil">
-					<img src={github} />
-				</a></div>
-				<div><a href="https://www.kataymeloglu.com">
-					<img src={kataymeloglu} />
-				</a></div>
-				<div><a href="https://mn350.org">
-					<img src={mn350} />
-				</a></div>
-				<div><a href="https://mn350action.org">
-					<img src={mn350action} />
-				</a></div>
-				<img src={federalcourt} />
-				<img src={qomb} />
-			</div>
-      	</div>
-*/}      	
+		<PortfolioScroll />      	
 	</div>
 );
 
