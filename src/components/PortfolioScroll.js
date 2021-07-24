61ddf9ac7299198b3f38392e763d053293ea7b25
John McNeil <john@johnmcneil.me>
2021-07-23 23:01:08 -0500

    passing props to portfolio scroll items

--- src/components/PortfolioScroll.js
+++ src/components/PortfolioScroll.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PortfolioScrollItem from './PortfolioScrollItem';
 
-const PortfolioScroll = ({ list = [] }) => {
+const PortfolioScroll = (props) => {
 	return(
 		<div id="portfolioScroll" className="row">
 			<div className="img-container col-md-6 offset-md-3">
-				<PortfolioScrollItem />
+				<PortfolioScrollItem data={props.data[0]} />
+				<PortfolioScrollItem data={props.data[1]} />
+				<PortfolioScrollItem data={props.data[2]} />
+				<PortfolioScrollItem data={props.data[3]} />
+				<PortfolioScrollItem data={props.data[4]} />
+				<PortfolioScrollItem data={props.data[5]} />
+				<PortfolioScrollItem data={props.data[6]} />
 			</div>
 		</div>
 	);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import CodeCloud from '../components/CodeCloud';
 import PortfolioScroll from '../components/PortfolioScroll';
-
+import chessgraphs from '../img/chessgraphs.png';
+import johnmcneilme from '../img/johnmcneilme.png';
+import kataymeloglu from '../img/kataymeloglu.png';
+import mn350 from '../img/mn350.png';
+import mn350action from '../img/mn350action.png';
+import github from '../img/github.png';
+import qomb from '../img/qomb.png';
+import federalcourt from '../img/federalcourt-screenshot.png';
 
 const title = "<John McNeil />";
 const subtitle = "{full-stack web development}";
 
 
-const HomePage = () => (
+const portfolioData = [
+	{
+		src: chessgraphs, 
+		href: "https://www.chessgraphs.com"
+	},
+	{
+		src: github, 
+		href: "https://github.com/johnmcneil"
+	},
+	{
+		src: kataymeloglu, 
+		href: "https://www.kataymeloglu.com"
+	},
+	{
+		src: mn350, 
+		href: "https://mn350.org"
+	},
+	{
+		src: mn350action, 
+		href: "https://mn350action.org"
+	},
+	{
+		src: qomb, 
+		href: ""
+	},
+	{
+		src: federalcourt, 
+		href: ""
+	},
+];
+
+const HomePage = (props) => (
 	<div className="container">
 		<div className="wordcloud-row row">
 			<CodeCloud className="offset-md-0" />
@@ -16,7 +54,7 @@ const HomePage = () => (
 			<h2>{title}</h2>
 			<h3>{subtitle}</h3>
 		</div>
-		<PortfolioScroll />      	
+		<PortfolioScroll data={portfolioData}/>      	
 	</div>
 );
 
