c8972753256da4098102ebc6f3c92fc4b1259ded
John McNeil <john@johnmcneil.me>
2021-09-08 16:40:02 -0500

    setting up ghg page

--- src/pages/GhgPage.js
+++ src/pages/GhgPage.js
@@ -3,5 +3,10 @@ import LineChart from '../components/LineChart.js';
 import EmissionsInfo from '../components/LineChart.js';
 
 export default function GhgPage() {
-	return <h1>GhgPage</h1>
+	return ( 
+		<>
+			<h1>GhgPage</h1>
+			<LineChart />
+		</>
+	);
 }
