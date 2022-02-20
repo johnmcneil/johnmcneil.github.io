ddb45e99e31fa0e412fe22ced23285492b9fc3dd
John McNeil <john@johnmcneil.me>
2022-02-19 21:40:44 -0600

    commiting everything in src directory

--- src/pages/SentinelSP5Page.js
+++ src/pages/SentinelSP5Page.js
@@ -4,13 +4,15 @@ import SentinelSP5SearchForm from '../components/SentinelSP5SearchForm';
 
 export default function SentinelSP5Page() {
 	const [country, setCountry] = useState("us");
+	const [stateUsa, setStateUsa] = useState("mn");
 	const [gas, setGas] =  useState("methane");
 	const [begin, setBegin] = useState("2020-02-01");
 	const [end, setEnd] = useState("2021-01-31");
 
-	function handleSearch(country, gas, begin, end) {
-		console.log("search function", country, gas, begin, end);
+	function handleSearch(country, stateUsa, gas, begin, end) {
+		console.log("search function", country, stateUsa, gas, begin, end);
 		setCountry(country);
+		setStateUsa(stateUsa);
 		setGas(gas);
 		setBegin(begin);
 		setEnd(end);
